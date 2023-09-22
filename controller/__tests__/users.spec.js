82e1275add5f9d897dd47e9c10863327bbca29be
Arely Gonzalez <arely.web@gmail.com>
2023-09-22 08:51:04 -0600

    modificaciones para hacer test unitario

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,9 +1,38 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
 const {
   getUsers,
 } = require('../users');
 
-describe('getUsers', () => {
-  it('should get users collection', (done) => {
-    done();
+describe('GET /user', function() {
+  it('responds with json', function(done) {
+    request(app)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
 });
+describe('POST /users', function() {
+  it('responds with json', function(done) {
+    request(app)
+      .post('/users')
+      .send({name: 'john'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
+
+// describe('getUsers', () => {
+//   it('should get users collection', (done) => {
+//     done();
+//   });
+// });
