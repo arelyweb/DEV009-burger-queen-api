a45de7421170649eebba3a8cf05abc1db2b04efe
Arely Gonzalez <arely.web@gmail.com>
2023-09-25 08:31:03 -0600

    intento de hacer pruebas

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,38 +1,46 @@
-const request = require('supertest');
+const mongoose = require('mongoose');
+const User = require('../../model/user.model');
 const express = require('express');
-
-const app = express();
+const res = require('./util/response')
+const {MongoClient} = require('mongodb');
 
 const {
   getUsers,
-} = require('../users');
+} = require('../users.controller');
 
-describe('GET /user', function() {
-  it('responds with json', function(done) {
-    request(app)
-      .get('/user')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
-  });
-});
-describe('POST /users', function() {
-  it('responds with json', function(done) {
-    request(app)
-      .post('/users')
-      .send({name: 'john'})
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function(err, res) {
-        if (err) return done(err);
-        return done();
-      });
-  });
-});
 
-// describe('getUsers', () => {
-//   it('should get users collection', (done) => {
-//     done();
-//   });
-// });
+const queryUsers = { _page: '3', _limit: '1' };
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = await connection.db(globalThis.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = {_id: 'some-user-id', email: 'admin@localhost.com'};
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(mockUser);
+  });
+  // describe('getUsers', () => {
+  //   it('should get users collection', (done) => {
+  //     const result = getUsers(queryUsers, resp, next);
+  //     expect(result).toBe(res.getusersRes);
+  //     done();
+  //   });
+  // });
+});
